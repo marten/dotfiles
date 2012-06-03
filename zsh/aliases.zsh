a3f58583ed47ab75ae8f323fb0c5df72c3fb3bd8
Marten Veldthuis <marten@veldthuis.com>
2012-06-03 14:08:17 +0200

    Updates to zsh modularity

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias ..='cd ..'
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
+alias r='rails'
+alias rl='tail -n500 -f log/development.log'
+alias be='bundle exec'
+alias t='soundcheck'
+alias e=emacsclient
---
+++ zsh/config.zsh
@@ -0,0 +1,32 @@
+autoload -U colors
+colors
+
+setopt promptsubst
+autoload -U promptinit
+promptinit
+prompt marten
+
+# Automatically pushd
+setopt autopushd
+
+export GEOLOQI_TOKEN="6d67-6079eaaa6955cb62838be656e36cd00398d7ec55"
+
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
+export MAKEFLAGS="-j8"
+export EDITOR=vim
+
+# Setup autojump if we have it (https://github.com/joelthelion/autojump/wiki/)
+[[ -s /etc/profile.d/autojump.zsh ]]     && source /etc/profile.d/autojump.zsh
+if [ -f /usr/local/etc/autojump ]; then
+    . /usr/local/etc/autojump
+fi
+
+# Setup nvm if we have it
+[[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"
+
+# Setup my own bin as the first place to look
+export PATH=$HOME/bin:$PATH
+
+function dburl() {
+  dropbox puburl ~/Dropbox/Public/$1.png | pbcopy
+}

322b02cdf0f1451145bdef226c3984ff9d9b6d25
Marten Veldthuis <marten@veldthuis.com>
2013-11-06 13:33:49 +0100

    Fix home/end keys not being what my mind expects

--- emacs/emacs.d.symlink/init/init-keys.el
+++ emacs/emacs.d.symlink/init/init-keys.el
@@ -2,4 +2,8 @@
 (define-key global-map (kbd "s-=") 'text-scale-increase)
 (define-key global-map (kbd "s--") 'text-scale-decrease)
 
+;; Navigation
+(define-key global-map (kbd "<home>") 'move-beginning-of-line)
+(define-key global-map (kbd "<end>") 'end-of-line)
+
 (provide 'init-keys)

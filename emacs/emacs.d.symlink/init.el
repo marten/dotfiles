3380c6e8522f54de3795ef8afe926920b76a58f0
Marten Veldthuis <marten@veldthuis.com>
2013-11-07 22:04:10 +0100

    Add monokai

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -6,23 +6,25 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/emacs-powerline"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/smartparens"))
 
+(load-theme 'monokai t)
+
 (cd "~/")
 
 (mapcar 'require
-	'(init-core
-    ; init-theme
-    init-keys
-    init-ido
-    init-autocomplete
-    init-highlight-indentation
-    init-dash-at-point
-    ; init-find-file-in-project
-    init-projectile
-    init-powerline
-    init-smartparens
-	  init-evil
-    init-ruby
-	  ))
+ 	'(init-core
+     ; init-theme
+     init-keys
+     init-ido
+     init-autocomplete
+     init-highlight-indentation
+     init-dash-at-point
+     ; init-find-file-in-project
+     init-projectile
+     init-powerline
+     init-smartparens
+     init-evil
+     init-ruby
+ 	  ))
 
 
 (custom-set-variables
@@ -30,6 +32,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" default)))
  '(fill-column 85))
 
 (custom-set-faces

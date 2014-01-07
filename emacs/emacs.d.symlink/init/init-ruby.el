9a82c98d91355eeb4bf4fb8c34b07f8675d871fb
Marten Veldthuis <marten@veldthuis.com>
2014-01-07 09:30:49 +0100

    updates

--- emacs/emacs.d.symlink/init/init-ruby.el
+++ emacs/emacs.d.symlink/init/init-ruby.el
@@ -1,13 +1,13 @@
-(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for Ruby files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+; (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for Ruby files" t)
+; (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+;(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+;(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+;(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+;(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+;(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
 
-(add-hook 'enh-ruby-mode-hook '(lambda ()
-                                 (local-set-key (kbd "RET") 'newline-and-indent)))
+;(add-hook 'enh-ruby-mode-hook '(lambda ()
+;                                 (local-set-key (kbd "RET") 'newline-and-indent)))
 
 
 (sp-pair "{" nil :actions :rem)

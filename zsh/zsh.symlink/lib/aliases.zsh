69bb1c540b2559e909e35b35c09ace43d99efd5e
Marten Veldthuis <marten@veldthuis.com>
2016-07-12 15:00:05 +0100

    Updates

--- zsh/zsh.symlink/lib/aliases.zsh
+++ zsh/zsh.symlink/lib/aliases.zsh
@@ -23,6 +23,11 @@ alias sl=ls # often screw this up
 alias vim=nvim
 alias vi=nvim
 alias v=nvim
+alias git-up="~/.rvm/wrappers/default/git-up"
 
 alias afind='ack-grep -il'
 
+alias psql-panoptes-staging='psql -h panoptes-staging-new.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user panoptes panoptes'
+alias psql-panoptes-production='psql -h panoptes-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user readonly panoptes'
+alias psql-edu-staging='psql -h education.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_staging education_staging'
+alias psql-edu-production='psql -h education.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_production education_production'

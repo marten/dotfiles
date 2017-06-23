d494c1d3bb169b77b004323bcc3374960ea90dc7
Marten Veldthuis <marten@veldthuis.com>
2017-06-23 11:28:19 +0100

    remove nvm, switch to asdf

--- zsh/zsh.symlink/lib/aliases.zsh
+++ zsh/zsh.symlink/lib/aliases.zsh
@@ -20,14 +20,22 @@ alias l='ls -la'
 alias ll='ls -l'
 alias sl=ls # often screw this up
 
-alias vim=nvim
-alias vi=nvim
-alias v=nvim
+#alias vim=nvim
+alias vim='emacsclient -nw'
+alias vi=vim
+alias v=vim
 alias git-up="~/.rvm/wrappers/default/git-up"
 
 alias afind='ack-grep -il'
 
 alias psql-panoptes-staging='psql -h panoptes-staging-new.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user panoptes panoptes'
 alias psql-panoptes-production='psql -h panoptes-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user readonly panoptes'
-alias psql-edu-staging='psql -h education.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_staging education_staging'
-alias psql-edu-production='psql -h education.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_production education_production'
+alias psql-panoptes-slave='psql -h panoptes-production-read1.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user readonly panoptes'
+
+alias psql-edu-staging='psql -h microservices-staging.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_staging education_staging'
+alias psql-edu-production='psql -h microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_production education_production'
+
+alias psql-caesar-staging='psql -h microservices-staging.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user caesar_staging caesar_staging'
+alias psql-caesar-production='psql -h microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user caesar_production caesar_production'
+
+alias psql-nero-production='psql -h microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user nero nero'

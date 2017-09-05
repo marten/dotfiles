e18507bd711ffdf6f97695112c545893f1655505
Marten Veldthuis <marten@veldthuis.com>
2017-09-05 10:45:31 +0100

    updates

--- zsh/zsh.symlink/lib/autojump.zsh
+++ zsh/zsh.symlink/lib/autojump.zsh
@@ -1,3 +1,5 @@
 if (( $+commands[brew] )) ; then
   [[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
 fi
+
+[[ -s /usr/share/autojump/autojump.sh ]] && . /usr/share/autojump/autojump.sh

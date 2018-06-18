e909923a17275992e3b6456d031c08ba95a5eddd
Marten Veldthuis <marten@veldthuis.com>
2018-06-18 15:58:16 +0100

    foo

--- zsh/zsh.symlink/lib/aliases.zsh
+++ zsh/zsh.symlink/lib/aliases.zsh
@@ -32,6 +32,8 @@ alias psql-panoptes-staging='psql -h panoptes-staging-new.cezuuccr9cw6.us-east-1
 alias psql-panoptes-production='psql -h panoptes-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user readonly panoptes'
 alias psql-panoptes-slave='psql -h panoptes-production-read1.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user readonly panoptes'
 
+alias psql-talk-production='psql -h panoptes-microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user talk_production talk_production'
+
 alias psql-edu-staging='psql -h microservices-staging.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_staging education_staging'
 alias psql-edu-production='psql -h microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user education_production education_production'
 
@@ -39,5 +41,7 @@ alias psql-caesar-staging='psql -h microservices-staging.cezuuccr9cw6.us-east-1.
 alias psql-caesar-production='psql -h microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user caesar_production caesar_production'
 
 alias psql-nero-production='psql -h microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user nero nero'
+
+alias psql-turk-production='psql -h microservices-production.cezuuccr9cw6.us-east-1.rds.amazonaws.com --user mechanical_zoo_production mechanical_zoo_production'
 
 alias sas='hermes exec StandaloneAppsSwarm -- docker '

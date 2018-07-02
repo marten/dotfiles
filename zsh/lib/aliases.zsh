a29ac473143918161ebdc1bae5c942ef3d24b1e2
Marten Veldthuis <marten@veldthuis.com>
2018-07-02 10:50:43 +0100

    Add sshuttle alias

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -25,6 +25,8 @@ alias e='emacsclient -nw'
 alias git-up="~/.rvm/wrappers/default/git-up"
 alias datagrip="/usr/local/datagrip/bin/datagrip.sh"
 
++alias beheer='sshuttle -r beheer11 10.128.0.0/9'
+
 alias afind='ack-grep -il'
 alias awssh="docker run -it --rm --volume $HOME/.ssh:/root/.ssh -e AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_SECURITY_TOKEN astopy/awssh"
 

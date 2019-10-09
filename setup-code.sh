d66f3c28634e7ead7e8c0093cfb2454018e59388
Marten Veldthuis <marten@veldthuis.com>
2019-10-09 20:49:39 +0200

    add code libs

---
+++ setup-code.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+code --install-extension castwide.solargraph
+code --install-extension deerawan.vscode-dash
+code --install-extension eamodio.gitlens
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension malt03.rails-rspec-file-toggle
+code --install-extension ms-python.python
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension rebornix.ruby
+code --install-extension shardulm94.trailing-spaces
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension syler.sass-indented
+code --install-extension vayan.haml
+code --install-extension vscodevim.vim
+code --install-extension yzhang.markdown-all-in-one

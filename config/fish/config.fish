4dca46ec2323a49912cb6b7076aebc10d8822da7
Marten Veldthuis <marten@veldthuis.com>
2020-03-11 09:46:38 +0100

    updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,12 +5,6 @@ if [ -d ~/bin ]
   set PATH $HOME/bin $PATH
 end
 
-if [ -d ~/.rbenv ]
-  set PATH $HOME/.rbenv/bin $PATH
-  set PATH $HOME/.rbenv/shims $PATH
-  rbenv rehash >/dev/null ^&1
-end
-
 if [ -d $HOME/.emacs.d/bin ]
   set PATH $HOME/.emacs.d/bin $PATH
 end
